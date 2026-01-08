fc3b66a351bc051d2fa21b76c80721dc8634caad
fernandamercado-commits <fernanda.mercado@ucb.edu.bo>
2026-01-08 12:29:53 -0400

    primer código python

---
+++ primer_código_python.py
@@ -0,0 +1,17 @@
+x = 5
+y=10
+resultado = x + y
+print("Hola, Fernanda el resultado es: ")
+print(resultado)
+
+print("---------------------------------")
+
+print("Calcular el área de un rectángulo")
+base = 10
+altura = 57
+area_rectangulo = base * altura
+print(area_rectangulo)
+
+print("---------------------------------")
+
+print(7*8)
